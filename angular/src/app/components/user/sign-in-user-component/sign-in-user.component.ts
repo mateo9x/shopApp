50b3115f9fcf69b17ea5a1a9115a327538453541
Mateusz <mtpala@interia.pl>
2021-09-05 20:12:07 +0200

    poprawki

--- angular/src/app/components/user/sign-in-user-component/sign-in-user.component.ts
+++ angular/src/app/components/user/sign-in-user-component/sign-in-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/components/user/user.model';
 import { UserService } from 'src/app/components/user/user.service';
@@ -17,28 +18,28 @@ export class SignInUserComponent implements OnInit {
   username: string;
   password: string;
 
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private userService: UserService, private router: Router, private messageService: MessageService) { }
 
   ngOnInit() {
 
   }
 
   onSignInButton() {
-      const userObj = {
-        username: this.username,
-        password: this.password
-      }
-
+    if (this.username !== undefined && this.password !== undefined) {
+      console.log('username');
+    const userObj = {
+      username: this.username,
+      password: this.password
+    }
       this.userService.signinUser(userObj).subscribe((response) => {
-      if(response.token !== null){
-        
-      }
-      console.log('logowanie', response);
-        
+        this.router.navigate(['']);
+      }, (error) => {
+        this.messageService.add({ key: 'error', severity: 'error', summary: 'Nie poprawne dane logowania!' });
       });
+    }
   }
 
-  signUp(){
+  signUp() {
     this.router.navigate(['sign-up-user']);
   }
 
