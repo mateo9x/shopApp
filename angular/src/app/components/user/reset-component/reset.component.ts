f321d2d20fd6af9443e4467ca8a39e501ed35835
Mateusz <mtpala@interia.pl>
2022-12-12 00:02:44 +0100

    Zmiana koloru przewodniego strony dla kilku komponentów

--- angular/src/app/components/user/reset-component/reset.component.ts
+++ angular/src/app/components/user/reset-component/reset.component.ts
@@ -1,27 +1,39 @@
-import { MessageService } from 'primeng/api';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { User } from 'src/app/components/user/user.model';
-import { UserService } from 'src/app/components/user/user.service';
+import {MessageService} from 'primeng/api';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {UserService} from 'src/app/components/user/user.service';
 
 @Component({
   selector: 'reset-password',
   templateUrl: './reset.component.html',
   styleUrls: ['./reset.component.scss']
 })
-export class ResetPasswordComponent implements OnInit {
+export class ResetPasswordComponent {
 
-  user: User = new User;
+  mail: string;
+  doesUserExistsInDatabase = false;
 
-  constructor(private userService: UserService, private router: Router, private messageService: MessageService) { }
-
-  ngOnInit() { }
+  constructor(private userService: UserService, private router: Router, private messageService: MessageService) {
+  }
 
   resetButton() {
-    if (this.user.mail !== undefined && this.user.mail !== undefined) {
-      this.userService.resetPassword(this.user).subscribe((response) => {
-        this.messageService.add({ key: 'success', severity: 'success', summary: 'Wysłano link do zresetowania hasła na podany adres e-mail'});
+    this.userService.doesUserWithEmailExists(this.mail).subscribe((response) => {
+      this.doesUserExistsInDatabase = response;
+    });
+    if (this.doesUserExistsInDatabase) {
+      this.userService.resetPassword(this.mail).subscribe((response) => {
+        this.messageService.add({
+          key: 'success',
+          severity: 'success',
+          summary: 'Wysłano link do zresetowania hasła na podany adres e-mail'
+        });
         this.router.navigate(['sign-in-user']);
+      });
+    } else {
+      this.messageService.add({
+        key: 'error',
+        severity: 'error',
+        summary: 'Taki użytkownik nie istnieje w bazie danych!'
       });
     }
   }
